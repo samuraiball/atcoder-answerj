df11b1f6820140a5f6ca3e48bfa89b8f76f33736
hirooka <yuy.hirohiro@gmail.com>
2019-05-24 00:58:12 +0900

    add

---
+++ src/main/java/ChangingACharacter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ChangingACharacter {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String length = s.next();
+        String targetNum = s.next();
+        String stringValue = s.next();
+        int a = Integer.parseInt(targetNum) - 1;
+        char[] carVals = stringValue.toCharArray();
+        carVals[a] =
+                Character.toLowerCase(carVals[a]);
+        System.out.println(String.valueOf(carVals));
+    }
+}
