cc76fcca4528355f4792fae76a3095452f6dba7d
tis305197 <password>
2019-05-29 14:58:12 +0900

    add: solved problems

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int N = Integer.parseInt(s.next());
+		int M = Integer.parseInt(s.next());
+
+
+	}
+}
